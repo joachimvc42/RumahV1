0ebcfb83abbef1031aae720ba6dc32f4eb7f2962
Joachim <joachim.vc42@gmail.com>
2026-01-01 10:12:55 +0100

    Disable Turbopack and fix alias resolution for Supabase

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: false,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = __dirname;
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
