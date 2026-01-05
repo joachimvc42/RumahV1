039af4ef5503c0c8072e33f388e0863d288536e1
Joachim <joachim.vc42@gmail.com>
2026-01-05 10:37:35 +0100

    Fix tsconfig path alias

--- app/admin/rentals/[id]/page.tsx
+++ app/admin/rentals/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../../../../lib/supabaseClient';
 import { useParams, useRouter } from 'next/navigation';
 import ImageUploader from '@/components/admin/ImageUploader';
 
