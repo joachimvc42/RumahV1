e9c5cab1da7e7d23e7bd3fdc4e49f25f816cfe85
Joachim <joachim.vc42@gmail.com>
2026-01-11 07:32:53 +0100

    chore: remove legacy PHP files

--- admin/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// admin/index.php
-// Redirection simple vers la liste des villas
-
-header('Location: villas.php');
-exit;
--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<meta http-equiv='refresh' content='0;url=/admin/login.php'>";
-echo "<script>window.location.href='/admin/login.php';</script>";
-exit;
